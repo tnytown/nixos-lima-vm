0aff79d22ca6cedd2ce4548e98b6eececc537b75
Andrew Pan <a@tny.town>
2023-10-26 05:07:40 -0500

    fixup! fixup! flake: vendor lima-vm

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,24 @@
 { config, modulesPath, pkgs, lib, ... }:
 {
     imports = [
-        (modulesPath + "/profiles/qemu-guest.nix")
         ./lima-init.nix
     ];
 
-    boot.initrd.availableKernelModules = [ "virtiofs" ];
-    boot.loader.systemd-boot.enable = true;
+    boot.initrd.availableKernelModules = [ "virtiofs" "virtio_pci" "virtio_blk" "virtio_console" "virtio_net" "simplefb" "xhci_pci" "usbhid" ];
+    boot.initrd.systemd.enable = true;
+    boot.initrd.compressor = "cat";
+    boot.loader.systemd-boot = {
+        enable = true;
+        extraFiles = with pkgs; {
+            "shellaa64.efi" = edk2-uefi-shell.efi;
+        };
+        extraEntries = {
+            "shellaa64.conf" = ''
+              title EFI Shell
+              efi /shellaa64.efi
+            '';
+        }
+    };
     boot.loader.efi.canTouchEfiVariables = true;
     boot.kernelPackages = pkgs.linuxPackages_latest;
 
@@ -35,4 +47,6 @@
         vim
         direnv
     ];
+
+    nixpkgs.hostPlatform = "aarch64-linux";
 }
